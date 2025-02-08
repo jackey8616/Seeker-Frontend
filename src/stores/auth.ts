e8501dfce0ffa54a610fccd541560291f2347ff9
Clooooode <clode@clo5de.info>
2025-02-08 22:18:24 +0800

    feat(Auth): apply token data extraction
    Remove useless user_id from localStorage since backend can handle from JWT token.

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -5,13 +5,11 @@ import { useApi } from "@/composables/useApi"
 
 export const useAuthStore = defineStore("auth", {
     state: () => ({
-        user_id: localStorage.getItem("user_id"),
         access_token: localStorage.getItem('access_token'),
         endpoint: inject('endpoint'),
         axios: useApi(true),
     }),
     getters: {
-        userId: (state) => state.user_id,
         token: (state) => state.access_token,
     },
     actions: {
@@ -21,16 +19,8 @@ export const useAuthStore = defineStore("auth", {
                 `${this.endpoint}/auth/oauth/google/verify`,
                 { code },
             )
-            const {
-                access_token,
-                user_id,
-            }: {
-                access_token: string;
-                user_id: string;
-            } = validate_response.data.data
-            this.user_id = user_id
+            const { access_token }: { access_token: string } = validate_response.data.data
             this.access_token = access_token
-            localStorage.setItem('user_id', user_id)
             localStorage.setItem('access_token', access_token)
         },
         async refresh() {
@@ -44,9 +34,7 @@ export const useAuthStore = defineStore("auth", {
             return this.access_token !== undefined && this.access_token !== null
         },
         logout() {
-            this.user_id = null;
             this.access_token = null;
-            localStorage.removeItem('user_id')
             localStorage.removeItem('access_token')
         }
     },
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -2,14 +2,12 @@
 import { onMounted, ref } from 'vue';
 
 import { useApi } from '@/composables/useApi';
-import { useAuthStore } from '@/stores/auth';
 
-const authStore = useAuthStore()
 const { axios } = useApi()
 const name = ref<string>('')
 
 onMounted(async () => {
-  const response = await axios.value.get(`/users/info/${authStore.userId}`)
+  const response = await axios.value.get(`/users/info`)
   name.value = response.data.name
 });
 </script>
