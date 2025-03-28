dcd75f4ba391d2542b708b22504f6773f0ff2ace
Clooooode <clode@clo5de.info>
2025-03-28 16:24:54 +0800

    fix(mail): list view abort actions while onMounted

--- src/views/MailListView.vue
+++ src/views/MailListView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 import type { Header, Item } from 'vue3-easy-data-table';
 import { useDate } from 'vuetify'
 import { useApi } from '@/composables/useApi'
@@ -14,10 +14,21 @@ const headers = ref<Header[]>([
 ])
 const mails = ref<Item[]>([])
 const nextPageToken = ref<string | null>(null)
+const abortController = ref<AbortController | null>(null)
 
 async function downloadMail(id: string): Promise<any> {
-  const response = await axios.value.get(`/mails/${id}`)
-  return response.data.data.mail
+  try {
+    const response = await axios.value.get(`/mails/${id}`, {
+      signal: abortController.value?.signal
+    })
+    return response.data.data.mail
+  } catch (error: any) {
+    if (error.name === 'AbortError') {
+      console.log('Request was aborted')
+      return null
+    }
+    throw error
+  }
 }
 
 function embedHtml(rawHtml: string) {
@@ -26,21 +37,45 @@ function embedHtml(rawHtml: string) {
 }
 
 onMounted(async () => {
-  const response = await axios.value.get("/mails/list")
-  const { data } = response.data
-  mails.value = data.mail_infos
-  nextPageToken.value = data.next_page_token
-
-  mails.value.map((mail) => {
-    downloadMail(mail.id).then((mail_data) => {
-      mail.sender = mail_data.sender;
-      mail.title = mail_data.title;
-      mail.date = mail_data.date;
-      mail.detailMail = mail_data;
-    }).catch((err) => {
-      console.error(`Download mail id ${mail.id} cause error`, err)
+  abortController.value = new AbortController()
+  
+  try {
+    const response = await axios.value.get("/mails/list", {
+      signal: abortController.value.signal
     })
-  })
+    const { data } = response.data
+    mails.value = data.mail_infos
+    nextPageToken.value = data.next_page_token
+
+    const downloadPromises = mails.value.map((mail) => {
+      return downloadMail(mail.id)
+        .then((mail_data) => {
+          if (mail_data) {
+            mail.sender = mail_data.sender;
+            mail.title = mail_data.title;
+            mail.date = mail_data.date;
+            mail.detailMail = mail_data;
+          }
+        })
+        .catch((err) => {
+          console.error(`Download mail id ${mail.id} cause error`, err)
+        })
+    })
+
+    await Promise.all(downloadPromises)
+  } catch (error: any) {
+    if (error.name === 'AbortError') {
+      console.log('List request was aborted')
+      return
+    }
+    console.error('Error fetching mail list:', error)
+  }
+})
+
+onUnmounted(() => {
+  if (abortController.value) {
+    abortController.value.abort()
+  }
 })
 </script>
 
